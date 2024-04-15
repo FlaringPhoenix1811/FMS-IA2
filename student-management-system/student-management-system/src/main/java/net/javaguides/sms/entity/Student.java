66145fad6e6f383d3a8fac3648a1b0efa9ef265a
Meet Jain <115941907+FlaringPhoenix1811@users.noreply.github.com>
2024-04-15 16:22:07 +0530

    Add files via upload

---
+++ student-management-system/student-management-system/src/main/java/net/javaguides/sms/entity/Student.java
@@ -0,0 +1,91 @@
+package net.javaguides.sms.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "students")
+public class Student {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "roll_no", nullable = false)
+	private String rollNo;
+	
+	@Column(name = "first_name", nullable = false)
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "branch", nullable = false)
+	private String branch;
+	
+	@Column(name = "section", nullable = false)
+	private String section;
+	
+	@Column(name = "email")
+	private String email;
+	
+	public Student() {
+		
+	}
+	
+	public Student(String rollNo, String firstName, String lastName, String branch, String section, String email) {
+		super();
+		this.rollNo = rollNo;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.branch = branch;
+		this.section = section;
+		this.email = email;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getRollNo() {
+		return rollNo;
+	}
+	public void setRollNo(String rollNo) {
+		this.rollNo = rollNo;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getBranch() {
+		return branch;
+	}
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+	public String getSection() {
+		return section;
+	}
+	public void setSection(String section) {
+		this.section = section;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
